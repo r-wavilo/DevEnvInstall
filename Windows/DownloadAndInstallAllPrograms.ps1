ac0bad7f76e2a0510e66b605ceadabca703d99f2
Wavilo <66049131+r-wavilo@users.noreply.github.com>
2026-04-07 10:47:09 -0700

    Delete Windows directory

--- Windows/DownloadAndInstallAllPrograms.ps1
+++
@@ -1,6 +0,0 @@
-./DownloadAndInstallProgram-chezmoi.ps1
-./DownloadAndInstallProgram-clangd.ps1
-./DownloadAndInstallProgram-fd.ps1
-./DownloadAndInstallProgram-ripgrep.ps1
-
-
--- Windows/DownloadAndInstallProgram-clangd.ps1
+++
@@ -1,4 +0,0 @@
-. $PSScriptRoot\DevEnvInstallUtil.ps1
-DownloadAndInstallProgram -sourceUrl "https://github.com/clangd/clangd/releases/download/20.1.8/clangd-windows-20.1.8.zip" -dstInstallDirName ".clangd" -dstBinDirName "bin"
-DownloadAndInstallProgram -sourceUrl "https://github.com/clangd/clangd/releases/download/20.1.8/clangd_indexing_tools-windows-20.1.8.zip" -dstInstallDirName ".clangd" -dstBinDirName "bin"
-
